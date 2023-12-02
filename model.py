ae9348b8204875609d51ec8b1654074f930e0dbd
XinyangLuo <118020047@link.cuhk.edu.cn>
2023-12-02 23:53:41 +0800

    majorize rank loss

--- model.py
+++ model.py
@@ -90,6 +90,6 @@ class MSEPlusRank(nn.Module):
     def forward(self, y_pred, y_true, mask):
         loss1 = self.mse(y_pred, y_true, mask)
         loss2 = self.rankloss(y_pred, y_true, mask)
-        coef = (loss1/loss2).detach()
-        loss = loss1 + self.lamda*coef*loss2
+        coef = (loss2/loss1).detach()
+        loss = coef*loss1 + self.lamda*loss2
         return loss, loss1, loss2
